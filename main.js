b0f847c07085f0f2254b070fa33ef08f0b001121
DangBaLoc <dangloc522@gmail.com>
2023-03-28 08:11:41 +0700

    fix size font description

--- main.js
+++ main.js
@@ -106,8 +106,6 @@ const map = L.map("map", config).setView([latDf, lngDf], 10);
 L.tileLayer(
   "https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token={accessToken}",
   {
-    attribution:
-      '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     minZoom: 7,
     maxZoom: 20,
     subdomains: "abcd",
